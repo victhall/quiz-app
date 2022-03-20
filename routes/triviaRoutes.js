7c78ef3e65fd4e071372e67e58c8218d5f1a84be
victhall <victoriahall.dev@gmail.com>
2022-03-20 00:17:34 -0400

    implement quiz editing

--- routes/triviaRoutes.js
+++ routes/triviaRoutes.js
@@ -3,28 +3,25 @@ const router = express.Router();
 const Trivia = require('../models/trivia')
 const Question = require('../models/questions')
 
-// const Score = require('../models/scores')
-
 //index
 router.get('/', async (req, res) => {
   const trivias = await Trivia.find({})
   res.render('trivia/index', { trivias })
-})
+});
 
 //new form
 router.get('/new', async (req, res) => {
   res.render('trivia/new')
-})
+});
 
+//post form
 router.post('/new', async (req, res) => {
-  Trivia.init();
-
   const trivia = new Trivia({
     title: req.body.title,
     description: req.body.description,
     is_public: req.body.is_public
   })
-  const questions = Question.insertMany([
+  Question.insertMany([
     { question: req.body.question, options: req.body.options, correct_option: req.body.correct_option, triviaId: trivia._id },
     { question: req.body.question2, options: req.body.options2, correct_option: req.body.correct_option2, triviaId: trivia._id },
     { question: req.body.question3, options: req.body.options3, correct_option: req.body.correct_option3, triviaId: trivia._id },
@@ -34,18 +31,43 @@ router.post('/new', async (req, res) => {
     { question: req.body.question7, options: req.body.options7, correct_option: req.body.correct_option7, triviaId: trivia._id },
     { question: req.body.question8, options: req.body.options8, correct_option: req.body.correct_option8, triviaId: trivia._id },
   ])
-
   await trivia.save();
-  console.log('NEWLY CREATED TRIVIA', trivia);
-  console.log('NEWLY CREATED QUESTION', questions);
   res.redirect(`/trivia/${trivia._id}`);
-  // res.send('FORM SENT')
 })
 
 //edit form
-router.get('/edit', async (req, res) => {
-  res.render('trivia/edit')
-})
+router.get('/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const trivia = await Trivia.findById(id);
+  const questions = await Question.find({ triviaId: id });
+  res.render('trivia/edit', { trivia, questions })
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const trivia = await Trivia.findByIdAndUpdate(id, { ...req.body });
+  const questions = await Question.find({ triviaId: id });
+  const example = questions[0]._id
+  const example2 = questions[1]._id
+  const example3 = questions[2]._id
+  const example4 = questions[3]._id
+  const example5 = questions[4]._id
+  const example6 = questions[5]._id
+  const example7 = questions[6]._id
+  const example8 = questions[7]._id
+
+  Question.findByIdAndUpdate(example, { $set: {question: req.body.question, options: req.body.options, correct: req.body.correct_options} }).exec();
+  Question.findByIdAndUpdate(example2, { $set: {question: req.body.question2, options: req.body.options2, correct: req.body.correct_option2} }).exec();
+  Question.findByIdAndUpdate(example3, { $set: {question: req.body.question3, options: req.body.options3, correct: req.body.correct_options3} }).exec();
+  Question.findByIdAndUpdate(example4, { $set: {question: req.body.question4, options: req.body.options4, correct: req.body.correct_options4} }).exec();
+  Question.findByIdAndUpdate(example5, { $set: {question: req.body.question5, options: req.body.options5, correct: req.body.correct_options5} }).exec();
+  Question.findByIdAndUpdate(example6, { $set: {question: req.body.question6, options: req.body.options6, correct: req.body.correct_options6} }).exec();
+  Question.findByIdAndUpdate(example7, { $set: {question: req.body.question7, options: req.body.options7, correct: req.body.correct_options7} }).exec();
+  Question.findByIdAndUpdate(example8, { $set: {question: req.body.question8, options: req.body.options8, correct: req.body.correct_options8} }).exec();
+
+  await trivia.save();
+  res.redirect(`/trivia/${trivia._id}`);
+});
 
 //show
 router.get('/:id', async (req, res) => {
@@ -60,10 +82,6 @@ router.get('/:id/play', async (req, res) => {
   const { id } = req.params;
   const trivia = await Trivia.findById(id);
   const questions = await Question.find({ triviaId: id })
-  // console.log('TRIVIA', trivia);
-  // console.log('TRIVIA TITLE', trivia[0].title);
-  console.log('QUESTIONS', questions)
-  // console.log('CORRECT ANSWER', correctAnswer);
   res.render('trivia/play', { questions, trivia })
 });
 
@@ -72,11 +90,7 @@ router.post('/:id/play', async (req, res, next) => {
   const trivia = await Trivia.findById(id);
   const questions = await Question.find({ triviaId: id })
   const answer = Object.values(req.body);
-  console.log('SELECTED ANSWER', answer);
-  // const correctAnswer = questions.map(question => question.correct_option);
-  // console.log('COORECT',correctAnswer)
   const correctAnswers = [];
-  const score = 0;
   for (let question of questions) {
     correctAnswers.push(question.correct_option)
   }
@@ -89,7 +103,6 @@ router.post('/:id/play', async (req, res, next) => {
   console.log('correct asnwers', correctAnswers)
   res.render('trivia/score', { trivia, count, correctAnswers })
 });
-
 
 
 module.exports = router;
